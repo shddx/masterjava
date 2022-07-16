e5411a86371968c623ee29225b617a3b0c078b27
maksim.rodionov <maksim.rodionov@gost-group.com>
2022-07-16 22:45:18 +0300

    lesson1 homework

--- src/main/java/ru/javaops/masterjava/matrix/MatrixUtil.java
+++ src/main/java/ru/javaops/masterjava/matrix/MatrixUtil.java
@@ -1,8 +1,12 @@
 package ru.javaops.masterjava.matrix;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Random;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Future;
+import java.util.stream.IntStream;
 
 /**
  * gkislin
@@ -14,8 +18,36 @@ public class MatrixUtil {
     public static int[][] concurrentMultiply(int[][] matrixA, int[][] matrixB, ExecutorService executor) throws InterruptedException, ExecutionException {
         final int matrixSize = matrixA.length;
         final int[][] matrixC = new int[matrixSize][matrixSize];
-
+        List<Future<int[]>> futures = new ArrayList<>();
+        int[][] matrixBTrans = new int[matrixSize][matrixSize];
+        for (int i = 0; i < matrixSize; i++) {
+            for (int j = 0; j < matrixSize; j++) {
+                matrixBTrans[i][j] = matrixB[j][i];
+            }
+        }
+        for (int[] row : matrixA) {
+            Future<int[]> future = executor.submit(() -> multiplyOneColumn(row, matrixBTrans));
+            futures.add(future);
+        }
+        for (int i = 0; i < futures.size(); i++) {
+            matrixC[i] = futures.get(i).get();
+        }
         return matrixC;
+    }
+
+    private static int[] multiplyOneColumn(int[] row, int[][] matrix) {
+        int size = row.length;
+        int[] result = new int[size];
+        for (int i = 0; i < size; i++) {
+            result[i] = multiplySingle(row, matrix[i]);
+        }
+        return result;
+    }
+
+    private static int multiplySingle(int[] column, int[] row) {
+        return IntStream.range(0, column.length)
+                .map(i -> column[i] * row[i])
+                .sum();
     }
 
     // TODO optimize by https://habrahabr.ru/post/114797/
@@ -23,13 +55,19 @@ public class MatrixUtil {
         final int matrixSize = matrixA.length;
         final int[][] matrixC = new int[matrixSize][matrixSize];
 
+        int[] column = new int[matrixSize];
         for (int i = 0; i < matrixSize; i++) {
+            for (int k = 0; k < matrixSize; k++) {
+                column[k] = matrixB[k][i];
+            }
+
             for (int j = 0; j < matrixSize; j++) {
+                int[] row = matrixA[j];
                 int sum = 0;
                 for (int k = 0; k < matrixSize; k++) {
-                    sum += matrixA[i][k] * matrixB[k][j];
+                    sum += row[k] * column[k];
                 }
-                matrixC[i][j] = sum;
+                matrixC[j][i] = sum;
             }
         }
         return matrixC;
